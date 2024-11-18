f68acaa17fdc1133ffb33054cb99803a409d0513
AiezerSantos <aiezer.santos01@gmail.com>
2024-11-17 23:08:01 -0300

    feat: implementado o metodo store no sales_controller, agora é possível criar novas vendas e relaciná-las a um determinado cliente pelo clientId.

---
+++ app/controllers/sales_controller.ts
@@ -0,0 +1,44 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+import Database from '@adonisjs/lucid/services/db'
+import Sale from '#models/sale'
+import SalesProducts from '#models/sales_products'
+import { DateTime } from 'luxon'
+import { calculateTotalPrice } from '../utils/calculate_total_price.js'
+
+export default class SalesController {
+  public async store({ request, response }: HttpContext) {
+    const trx = await Database.transaction()
+    try {
+      const { clientId, products } = request.only(['clientId', 'products'])
+
+      const totalPrice = await calculateTotalPrice(products)
+
+      const sale = await Sale.create(
+        {
+          clientId,
+          totalPrice,
+          saleDate: DateTime.now(),
+        },
+        { client: trx }
+      )
+
+      for (const prod of products) {
+        await SalesProducts.create(
+          {
+            saleId: sale.id,
+            productId: prod.productId,
+            quantity: prod.quantity,
+          },
+          { client: trx }
+        )
+      }
+
+      await trx.commit()
+      return response.status(201).json(sale)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(500).json({ error: 'Erro ao criar venda', details: error.message })
+    }
+  }
+}
